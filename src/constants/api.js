2d26436d2ca31e2a54334cd96963bb3ec38cf1c5
Vitalick37 <vit198837@mail.ru>
2023-04-15 10:37:52 +0300

    ref

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,16 +1,13 @@
-// общие константы
 
 export const HTTPS = 'https://';
 export const HTTP = 'http://';
 
-// для swapi
 export const SWAPI_ROOT = 'swapi.dev/api/';
 export const SWAPI_PEOPLE = 'people';
 export const SWAPI_PARAM_PAGE = '/?page=';
 
 export const API_PEOPLE = HTTPS + SWAPI_ROOT + SWAPI_PEOPLE + SWAPI_PARAM_PAGE;
 
-// для visualguide
 const GUIDE_ROOT_IMG = 'https://starwars-visualguide.com/assets/img/';
 const GUIDE_PEOPLE = 'characters';
 export const GUIDE_IMG_EXTENSION = '.jpg';
